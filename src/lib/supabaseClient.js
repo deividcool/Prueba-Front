8bb93c604a8f958f93ea47b0f51b9cb3f9297cba
Deivid Veloza <velozadeivid@gmail.com>
2024-09-26 11:28:19 -0500

    Update supabaseClient.js

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -1,8 +1,8 @@
 import { createClient } from '@supabase/supabase-js';
 
 
-const supabaseUrl = 'https://zqsjmmuebcuhsfjzkbcv.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inpxc2ptbXVlYmN1aHNmanprYmN2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjczMDI0MDIsImV4cCI6MjA0Mjg3ODQwMn0.aOIxAYqfYPWQyDhn5tyrRb8p4F_Q8KCqy9khkDVNgkI';
+const supabaseUrl = import.meta.env.PUBLIC_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.PUBLIC_SUPABASE_ANON_KEY;
 
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);

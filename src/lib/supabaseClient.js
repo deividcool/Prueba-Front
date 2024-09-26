e51dd005a81ad860ce11f266a42b512d2e916bd6
DeividWinnersMasterMind <deivid@winnersmastermind.com>
2024-09-26 11:46:01 -0500

    Update .env

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -1,8 +1,10 @@
 import { createClient } from '@supabase/supabase-js';
 
-
-const supabaseUrl = import.meta.env.PUBLIC_SUPABASE_URL;
-const supabaseAnonKey = import.meta.env.PUBLIC_SUPABASE_ANON_KEY;
-
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+
+// Para depurar
+console.log("SUPABASE_URL:", supabaseUrl);
+console.log("SUPABASE_ANON_KEY:", supabaseAnonKey);

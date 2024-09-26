7977a7a648cd1ad21779976873529d0f784ba6c1
Deivid Veloza <velozadeivid@gmail.com>
2024-09-26 11:05:36 -0500

    Update Register.jsx

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,5 +1,6 @@
 // src/pages/Register.js
 import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { supabase } from '../lib/supabaseClient';
 
 function Register() {
@@ -28,6 +29,10 @@ function Register() {
     }
   };
 
+  const handleLogin = async () => {
+    navigate('/')
+  };
+
   return (
     <main className='flex flex-col items-center justify-center h-screen dark:bg-gray-900'>
         <section className='flex flex-col items-center justify-around w-[350px] h-80 bg-gray-200 rounded-lg py-2 pb-0 px-5 shadow-2xl sm:w-[400px] sm:py-6'>
@@ -52,7 +57,9 @@ function Register() {
                 className='flex w-full rounded-lg px-5 py-4'
               />
               <button type="submit" className='flex px-8 py-2 rounded-lg bg-pretty text-white bg-purple-900 uppercase font-semibold'>Register</button>
-              <a href="/" className='flex w-full items-start justify-end text-cyan-600'>loguearse ahora</a>
+             <button onClick={handleLogin} className='flex w-full items-start justify-end text-cyan-600'>
+                loguearse ahora
+              </button>
             </form>
           )}
         </section>
